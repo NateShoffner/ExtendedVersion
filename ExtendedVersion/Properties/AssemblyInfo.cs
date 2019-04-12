e8894a63b93acbaf94e4a7eaae5c98b4464fec61
Nate Shoffner <nate.shoffner@gmail.com>
2019-04-12 05:24:46 -0400

    Bumped .NET Framework to 4.6.1

--- ExtendedVersion/Properties/AssemblyInfo.cs
+++ ExtendedVersion/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nate Shoffner")]
 [assembly: AssemblyProduct("ExtendedVersion")]
-[assembly: AssemblyCopyright("Copyright © Nate Shoffner 2017")]
+[assembly: AssemblyCopyright("Copyright © Nate Shoffner 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
-[assembly: AssemblyInformationalVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyInformationalVersion("1.0.3.0")]
